33c0787c2ea1fa9f41d42d0f2615495f62163e4b
Arpan Mukherjee <arpan.mukherjee.pro@gmail.com>
2019-10-07 05:06:38 +0530

    Tester code updated

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # ifp-assessment-tester
+
+Assuming you are on a Linux System, you probably have Python 3 installed (And PIP as well)
+
+```
+pip3 install -r requirements.txt
+```
+
+Setup settings.json
+
+```
+{
+	"username": "jane",
+	"password": "123456789",
+	"server_url": "http://localhost:8000",
+	"interval": 1
+}
+
+
+```
+Above keys are required! Kindly point to your relavent server. Username/Password doesn't matter as long as they are valid ones. Required to obtain token.
+
+```
+python3 autopost.py
+```
--- autopost.py
+++ autopost.py
@@ -6,24 +6,44 @@ from dateutil.tz import gettz
 import time
 
 statusList = ['online', 'offline']
-authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRGV0YWlscyI6eyJ1c2VyaWQiOjQsInVzZXJuYW1lIjoiamFuZSJ9LCJpYXQiOjE1NzAzNTgzMTMsImV4cCI6MTU3MDM2MDExM30.QcXzI2b_huayvkiDsrFag5HQjVEwRuK4A5ZPD20BLgc'
+f = open('settings.json', "r")
+
+settings = json.loads(f.read())
+
+response = requests.post(url=settings['server_url']+'/login', data=settings)
+authResponse = response.json()
+
+# print(authResponse)
+
+authToken = authResponse['token']
 headers = {'Authorization': 'Bearer ' + authToken}
 
-while True:
-    chosenStatus = random.choice(statusList)
-    randomLoad = float(random.uniform(15.0, 50.5))
-    currentDatetime = datetime.now(gettz("Asia/Kolkata")).isoformat()
+deviceId = input('Enter deviceId whose data is to be spammed : ')
 
-    data = {
-        'deviceId': 'S10',
-        'status': chosenStatus,
-        'time': currentDatetime,
-        'load': randomLoad,
-    }
-    
-    response = requests.post(url='http://localhost:5000/monitor',
-                data=data,
-                headers=headers
-            )
-    print(response.json)
-    time.sleep(1);    
+while True:
+    try:
+        chosenStatus = random.choice(statusList)
+        randomLoad = float(random.uniform(15.0, 50.5))
+        currentDatetime = datetime.now(gettz("Asia/Kolkata")).isoformat()
+
+        data = {
+            'deviceId': deviceId,
+            'status': chosenStatus,
+            'time': currentDatetime,
+            'load': randomLoad,
+        }
+        
+        response = requests.post(url=settings['server_url']+'/monitor',
+                    data=data,
+                    headers=headers
+                )
+        print(response.json())
+        print("Press Ctrl+C to exit")
+        time.sleep(settings['interval'])
+        
+    except KeyboardInterrupt:
+        exit()
+
+
+
+
